352d92ee071d93c85072de96f200bde061793d83
mahmoud <malawad@dmind.it>
2023-01-05 17:58:27 +0100

    refactor some data

---
+++ data/user.js
@@ -0,0 +1,17 @@
+const users = [
+    root={
+        id: "2feb4b2a-8e32-4294-a32d-82855c965b7b",
+        groups: [
+          admin = {
+
+          },
+        ],
+        username: "root",
+        password: "55ea6cbb4ad36f3b167acd8e41b91923fdb53f6ff59c845dd322a2a3d754a238"
+      },
+    
+];
+
+module.exports = {
+    users
+}
